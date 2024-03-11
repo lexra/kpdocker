10665c6a844f3ab55124e3bc246a090186c15918
lexra <jasonc@mail2000.com.tw>
2024-03-11 15:40:00 +0800

    pip3 install gdown==5.1.0

--- run.sh
+++ run.sh
@@ -14,7 +14,6 @@ curl https://registry.hub.docker.com/v2/repositories/kneron/toolchain/tags | yq 
 [ -e /var/run/docker.sock ] && \
 	sudo chmod 777 /var/run/docker.sock
 
-
 ###########################################################
 # docker_mount
 ###########################################################
@@ -29,6 +28,7 @@ sudo docker rmi -f $(sudo docker images -a | grep "^${REPOSITORY}" | awk '{print
 ###########################################################
 # docker run
 ###########################################################
+pip3 install gdown==5.1.0
 [ ! -e examples/wheelchair/datasets.zip ] && gdown https://drive.google.com/uc?id=1uSpN-bDlX9wG66K36yuscewB58pFnpbz -O examples/wheelchair/
 [ ! -e examples/wheelchair/wheelchair.weights ] && gdown https://drive.google.com/uc?id=1K2fzXOUwuBjdBll3pHaldvqV41Rujsa_ -O examples/wheelchair/
 rm -rfv examples/wheelchair/*tmp
